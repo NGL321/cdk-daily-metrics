4fc6208a583997741cadfd03f6616f1cd2be38a4
Somaya Bounouar <somayb@amazon.com>
2020-01-17 15:51:15 -0800

    create initial semi-automated daily-metrics

--- bin/daily-metrics.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { DailyMetricsStack } from '../lib/daily-metrics-stack';
-
-const app = new cdk.App();
-new DailyMetricsStack(app, 'DailyMetricsStack');
